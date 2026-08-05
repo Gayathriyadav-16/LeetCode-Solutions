f916a2a4243ce9524546cf7fb1ee61fb54309524
Golla Gayathri yadav <gollagayathriyadav353@gmail.com>
2026-08-05 18:42:55 +0530

    Time: 3 ms (86.1%), Space: 43 MB (93.6%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,7 +1,29 @@
 import java.util.Stack;
-class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+ class Solution {
+     public boolean isValid(String s) {
+//         Stack<Character> st = new Stack<>();
+//         char ar[] = s.toCharArray();
+//         for(int i=0;i<ar.length;i++){
+//             char c = ar[i];
+//             if(c == '(' || c == '[' || c == '{'){
+//                 st.push(c);
+//             }
+//             else{
+//                 if(st.isEmpty()){
+//                     return false;
+//                 }
+//                 else{
+//                     char t = st.pop();
+//                     if(c == ')' && t != '(' || c == ']' && t != '[' || c == '}' && c == '{'){
+//                         return false;
+//                     }
+//                 }
+//             }
+//         }
+//         return st.isEmpty();
+//     }
+// }
+Stack<Character> stack = new Stack<>();
         char ar[] = s.toCharArray();
         for(int i=0;i<ar.length;i++) {
              char ch = ar[i];
@@ -22,4 +44,4 @@ class Solution {
         }
         return stack.isEmpty();
     }
-}
+ }
