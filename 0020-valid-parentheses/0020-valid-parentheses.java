7c4822833995d27a46acf459be4d6b1f6d0783cd
Golla Gayathri yadav <gollagayathriyadav353@gmail.com>
2026-08-05 18:47:40 +0530

    Time: 3 ms (86.1%), Space: 42.8 MB (98.38%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,47 +1,25 @@
 import java.util.Stack;
  class Solution {
      public boolean isValid(String s) {
-//         Stack<Character> st = new Stack<>();
-//         char ar[] = s.toCharArray();
-//         for(int i=0;i<ar.length;i++){
-//             char c = ar[i];
-//             if(c == '(' || c == '[' || c == '{'){
-//                 st.push(c);
-//             }
-//             else{
-//                 if(st.isEmpty()){
-//                     return false;
-//                 }
-//                 else{
-//                     char t = st.pop();
-//                     if(c == ')' && t != '(' || c == ']' && t != '[' || c == '}' && c == '{'){
-//                         return false;
-//                     }
-//                 }
-//             }
-//         }
-//         return st.isEmpty();
-//     }
-// }
-Stack<Character> stack = new Stack<>();
+        Stack<Character> st = new Stack<>();
         char ar[] = s.toCharArray();
-        for(int i=0;i<ar.length;i++) {
-             char ch = ar[i];
-            if (ch == '(' || ch == '{' || ch == '[') {
-                stack.push(ch);
-            } 
-            else {
-                if (stack.isEmpty()) {
+        for(int i=0;i<ar.length;i++){
+            char c = ar[i];
+            if(c == '(' || c == '[' || c == '{'){
+                st.push(c);
+            }
+            else{
+                if(st.isEmpty()){
                     return false;
                 }
-                char top = stack.pop();  
-                if ((ch == ')' && top != '(') ||
-                (ch == '}' && top != '{') ||
-                (ch == ']' && top !='[')) {
+                char t = st.pop();
+                if(c == ')' && t != '(' ||
+                   c == ']' && t != '[' || 
+                   c == '}' && t != '{'){
                     return false;
                 }
             }
         }
-        return stack.isEmpty();
+        return st.isEmpty();
     }
- }
+}
