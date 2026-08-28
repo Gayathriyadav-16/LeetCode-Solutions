cd211a807e0eb7c2a9e4f0f934d8b3948cf3ff13
Golla Gayathri yadav <gollagayathriyadav353@gmail.com>
2026-08-28 11:50:23 +0530

    Time: 0 ms (100%), Space: 42 MB (52.49%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static int solve(int[] dp,int n){
+        if(n<=1){
+            return n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n] = solve(dp,n-1)+solve(dp,n-2);
+        return dp[n];
+    }
+    public int fib(int n) { 
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(dp,n);
+    }
+
+}
